a37eaf435044f512c83869e1b440f88cac30e857
cameliabacaoanu <bacaoanucamelia@yahoo.com>
2017-01-05 10:46:02 +0200

    Schelet pentru jocul snake

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+void playMenu(){
+
+    }
+
+void scoreMenu(){
+
+}
+
+void Draw()
+{
+
+}
+void Input()
+{
+
+}
+void Logic()
+{
+
+}
+void startGame()
+{
+}
+
+void Setup()
+{
+}
+
+int main (){
+cout<<"BUN VENIT!!!"<<endl;
+
+}
