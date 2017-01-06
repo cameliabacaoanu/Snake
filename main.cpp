1c1c11a58dbf68631570c5c92c10e85da5cc3406
amarieioana <amarieioanaroxana@yahoo.com>
2017-01-06 23:59:06 +0200

    Meniu

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
-#include <conio.h>
+#include <windows.h>
+
 using namespace std;
+
 
 void playMenu(){
 
-    }
+}
 
 void scoreMenu(){
+
 
 }
 
 void Draw()
 {
+
 
 }
 void Input()
@@ -24,13 +28,43 @@ void Logic()
 }
 void startGame()
 {
+
+
 }
 
 void Setup()
 {
+  int menu;
+  cout<<"Selectati una din optiuni:"<<endl;
+  cout<<"1.Joc nou"<<endl;
+  cout<<"2. Tabela de scoruri"<<endl;
+  cout<<"0. Exit"<<endl;
+  cin>>menu;
+
+  switch (menu)
+	{
+	case 1:
+		playMenu();
+		startGame();
+		break;
+
+	case 2:
+           scoreMenu();
+            break;
+
+	case 0:
+      exit(0);
+		      break;
+      default:
+		      break;
+	}
 }
 
-int main (){
+int main ()
+{
 cout<<"BUN VENIT!!!"<<endl;
 
+Setup();
+
 }
+
